d1b4b609c1283de845b665c93abfb8a5d7c71eaf
Vivian <ndidiranjo@gmail.com>
2024-09-19 04:10:42 +0100

    style

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -72,7 +72,7 @@ const Login = () => {
             </div>
 
             <div className="flex items-center justify-between mt-4">
-              <span className="w-4/5 border-b dark:border-gray-900 md:w-1/4"></span>
+              <span className="w-2/5 border-b dark:border-gray-900 md:w-1/4"></span>
               <p className="text-xs px-2 text-but uppercase dark:text-gray-900">
                 Don’t have an account yet?
                 <Link

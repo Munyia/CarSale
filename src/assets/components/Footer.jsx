0b1f2f379546d39adac2ca9924455a3a7cdd7fe1
Vivian <ndidiranjo@gmail.com>
2024-09-19 02:01:53 +0100

    responsiveness

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -8,7 +8,6 @@ import {
   FaEnvelope,
   FaPhone,
 } from "react-icons/fa";
-import axios from "axios";
 
 const mockContactInfo = {
   phone: "+1 *****",
@@ -34,54 +33,89 @@ const Footer = () => {
   return (
     <footer className="bg-but text-white py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between gap-5">
-          <div className="flex flex-col w-full">
-            <h4 className="text-lg text-white  font-bold mb-4">Quick Links</h4>
-            <ul className="space-y-2 flex flex-col gap-2">
-              <li className="flex-grow min-w-[150px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {/* Quick Links */}
+          <div className="flex flex-col">
+            <h4 className="text-lg font-bold mb-4">Quick Links</h4>
+            <ul className="space-y-2">
+              <li>
                 <Link
                   to="/terms"
-                  className="text-white hover:text-black transition duration-300 block truncate"
+                  className="text-white hover:text-black transition duration-300 block"
                 >
                   Terms of Service
                 </Link>
               </li>
-              <li className="flex-grow min-w-[150px]">
+              <li>
                 <Link
                   to="/privacy"
-                  className="text-white hover:text-black transition duration-300 block truncate"
+                  className="text-white hover:text-black transition duration-300 block"
                 >
                   Privacy Policy
                 </Link>
               </li>
-              <li className="flex-grow min-w-[150px]">
+              <li>
                 <Link
                   to="/about"
-                  className="text-white hover:text-black transition duration-300 block truncate"
+                  className="text-white hover:text-black transition duration-300 block"
                 >
                   About Us
                 </Link>
               </li>
-              <li className="flex-grow min-w-[150px]">
+              <li>
                 <Link
                   to="/carlisting"
-                  className="text-white hover:text-black transition duration-300 block truncate"
+                  className="text-white hover:text-black transition duration-300 block"
                 >
                   Car Listing
                 </Link>
               </li>
-              <li className="flex-grow min-w-[150px]">
+              <li>
                 <Link
                   to="/checkout"
-                  className="text-white hover:text-black transition duration-300 block truncate"
+                  className="text-white hover:text-black transition duration-300 block"
                 >
                   Checkout
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="flex flex-col w-[15%] ml-5">
-            <h4 className="text-lg text-white font-bold mb-4">Contact Us</h4>
+           {/* Follow Us */}
+           <div className="flex flex-col">
+            <h4 className="text-lg font-bold mb-4 text-center lg:text-left">
+              Follow Us
+            </h4>
+            <div className="flex justify-center lg:justify-start space-x-4">
+              <Link
+                to="#"
+                className="text-black hover:text-white transition duration-300"
+              >
+                <FaFacebookF className="text-2xl hover:scale-150 transition-transform duration-300" />
+              </Link>
+              <Link
+                to="#"
+                className="text-black hover:text-white transition duration-300"
+              >
+                <FaTwitter className="text-2xl hover:scale-150 transition-transform duration-300" />
+              </Link>
+              <Link
+                to="#"
+                className="text-black hover:text-white transition duration-300"
+              >
+                <FaInstagram className="text-2xl hover:scale-150 transition-transform duration-300" />
+              </Link>
+              <Link
+                to="#"
+                className="text-black hover:text-white transition duration-300"
+              >
+                <FaLinkedin className="text-2xl hover:scale-150 transition-transform duration-300" />
+              </Link>
+            </div>
+          </div>
+
+          {/* Contact Us */}
+          <div className="flex flex-col">
+            <h4 className="text-lg font-bold mb-4">Contact Us</h4>
             <ul className="space-y-2">
               <li className="flex items-center space-x-2 hover:text-black">
                 <FaPhone className="text-xl" />
@@ -91,47 +125,16 @@ const Footer = () => {
                 <FaEnvelope className="text-xl" />
                 <span>{contactInfo.email}</span>
               </li>
-              <li className="text-white">{contactInfo.address}</li>
+              <li>{contactInfo.address}</li>
             </ul>
           </div>
+
+         
         </div>
 
-        <div className="flex flex-col">
-          <div className="text-center mb-6">
-            <h4 className="text-lg text-white font-bold mb-4">Follow Us</h4>
-            <div className="flex justify-center space-x-4">
-              <Link
-                to="#"
-                className="text-black hover:text-white transition duration-300"
-              >
-                <FaFacebookF className="text-2xl hover:scale-150 hover:text-white transition-transform duration-300" />
-              </Link>
-              <Link
-                to="#"
-                className="text-black hover:text-white transition duration-300"
-              >
-                <FaTwitter className="text-2xl hover:scale-150 hover:text-white transition-transform duration-300" />
-              </Link>
-              <Link
-                to="#"
-                className="text-black hover:text-white transition duration-300"
-              >
-                <FaInstagram className="text-2xl hover:scale-150 hover:text-white transition-transform duration-300" />
-              </Link>
-              <Link
-                to="#"
-                className="text-black hover:text-white transition duration-300"
-              >
-                <FaLinkedin className="text-2xl hover:scale-150 hover:text-white transition-transform duration-300" />
-              </Link>
-            </div>
-          </div>
-
-          <div className="text-center text-gray-300">
-            <p>
-              &copy; {new Date().getFullYear()} CarSale. All rights reserved.
-            </p>
-          </div>
+        {/* Footer Bottom */}
+        <div className="mt-6 text-center text-gray-300">
+          <p>&copy; {new Date().getFullYear()} CarSale. All rights reserved.</p>
         </div>
       </div>
     </footer>

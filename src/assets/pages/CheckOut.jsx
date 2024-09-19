5c034d8eaa9eeb55aa6f45777677bb02dbe41b72
Vivian <ndidiranjo@gmail.com>
2024-09-19 02:46:07 +0100

    responsiveness

--- src/assets/pages/CheckOut.jsx
+++ src/assets/pages/CheckOut.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
 const CheckOut = () => {
   const location = useLocation();
-  const { totalCost, selectedItems } = location.state || {}; // Destructure data from state
+  const { totalCost, selectedItems } = location.state || {}; 
 
   const [downPayment, setDownPayment] = useState(0);
   const [installmentPlan, setInstallmentPlan] = useState('');
@@ -12,7 +12,10 @@ const CheckOut = () => {
   const [monthlyInstallment, setMonthlyInstallment] = useState(0);
   const [paymentMethod, setPaymentMethod] = useState('');
 
-  // Function to calculate the monthly installment considering down payment
+  useEffect(() => {
+    document.title = 'Checkout - Car Dealer';
+  }, []);
+
   const calculateInstallment = (plan, downPayment) => {
     if (plan && totalCost) {
       const remainingAmount = totalCost - parseFloat(downPayment || 0);
@@ -21,12 +24,11 @@ const CheckOut = () => {
   };
 
   return (
-    <div>
+    <div className='bg-body'>
       <Header />
-      <div className="max-w-lg my-3 mx-auto p-6 shadow-2xl bg--600 bg-body shadow-slate-900 rounded-3xl">
-        <h1 className="text-2xl font-bold justify-center text-center mb-6 text-but">CheckOut Form</h1>
+      <div className="max-w-lg my-3 mx-auto p-6 shadow-2xl bg-body shadow-slate-900 rounded-3xl sm:max-w-xl lg:max-w-lg">
+        <h1 className="text-2xl font-bold justify-center text-center mb-6 text-but">Checkout Form</h1>
         <form className="space-y-4">
-          {/* Personal Information Fields */}
           <div>
             <label htmlFor="fullname" className="block text-sm font-semibold text-but mb-2">Full Name</label>
             <input
@@ -87,7 +89,6 @@ const CheckOut = () => {
             </select>
           </div>
           
-          {/* Payment and Installment Fields */}
           <div className="mt-6">
             <p className="text-lg font-semibold text-black">Payment Method</p>
           </div>
@@ -112,7 +113,6 @@ const CheckOut = () => {
             </select>
           </div>
 
-          {/* Conditionally show Down Payment only when an installment plan is selected */}
           {installmentPlan && (
             <div>
               <label htmlFor="downPayment" className="block text-sm font-semibold text-but mb-2">Down Payment</label>
@@ -129,60 +129,61 @@ const CheckOut = () => {
               />
             </div>
           )}
-          <div className="mt-6">
-            <div className="mt-4 space-y-4">
-              <div className="flex items-center">
-                <input
-                  type="radio"
-                  id="bankTransfer"
-                  name="paymentMethod"
-                  value="Bank Transfer"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                  onChange={() => setPaymentMethod('Bank Transfer')}
-                  required
-                />
-                <label htmlFor="bankTransfer" className="ml-2 text-gray-700">Bank Transfer</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="radio"
-                  id="crypto"
-                  name="paymentMethod"
-                  value="Cryptocurrency"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                  onChange={() => setPaymentMethod('Cryptocurrency')}
-                  required
-                />
-                <label htmlFor="crypto" className="ml-2 text-gray-700">Cryptocurrency</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="radio"
-                  id="creditCard"
-                  name="paymentMethod"
-                  value="Credit Card"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                  onChange={() => setPaymentMethod('Credit Card')}
-                  required
-                />
-                <label htmlFor="creditCard" className="ml-2 text-gray-700">Credit Card</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="radio"
-                  id="paypal"
-                  name="paymentMethod"
-                  value="PayPal"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                  onChange={() => setPaymentMethod('PayPal')}
-                  required
-                />
-                <label htmlFor="paypal" className="ml-2 text-gray-700">PayPal</label>
-              </div>
+          
+          {/* Payment Method Options */}
+          <div className="mt-6 space-y-4">
+            <div className="flex items-center">
+              <input
+                type="radio"
+                id="bankTransfer"
+                name="paymentMethod"
+                value="Bank Transfer"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                onChange={() => setPaymentMethod('Bank Transfer')}
+                required
+              />
+              <label htmlFor="bankTransfer" className="ml-2 text-gray-700">Bank Transfer</label>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="radio"
+                id="crypto"
+                name="paymentMethod"
+                value="Cryptocurrency"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                onChange={() => setPaymentMethod('Cryptocurrency')}
+                required
+              />
+              <label htmlFor="crypto" className="ml-2 text-gray-700">Cryptocurrency</label>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="radio"
+                id="creditCard"
+                name="paymentMethod"
+                value="Credit Card"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                onChange={() => setPaymentMethod('Credit Card')}
+                required
+              />
+              <label htmlFor="creditCard" className="ml-2 text-gray-700">Credit Card</label>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="radio"
+                id="paypal"
+                name="paymentMethod"
+                value="PayPal"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                onChange={() => setPaymentMethod('PayPal')}
+                required
+              />
+              <label htmlFor="paypal" className="ml-2 text-gray-700">PayPal</label>
             </div>
           </div>
-          <div>
-            <label htmlFor="deliveryOption" className="block text-but">Delivery Option</label>
+
+          <div className="mt-6">
+            <p className="text-lg font-semibold text-black">Delivery Option</p>
             <div className="mt-4 space-y-4">
               <div className="flex items-center">
                 <input
@@ -210,36 +211,23 @@ const CheckOut = () => {
               </div>
             </div>
           </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full py-2 bg-but text-white rounded-lg hover:bg-blue-700 hover:text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              Submit
-            </button>
+
+          {installmentPlan && (
+            <div>
+              <p className="text-xl font-bold text-but mt-4">
+                Monthly Installment: ${monthlyInstallment.toFixed(2)}
+              </p>
+            </div>
+          )}
+
+          <div className="mt-6 flex justify-center">
+            <Link to="/" className="text-but font-bold">
+              <button className="px-6 py-2 bg-blue-600 text-white rounded-lg">
+                Confirm Order
+              </button>
+            </Link>
           </div>
         </form>
-        
-        <div className='my-3 justify-center text-center'>
-          <Link className='w-full justify-center text-center px-5 py-2 bg-red-700 text-white rounded-lg hover:bg-but hover:text-black focus:outline-none focus:ring-2 focus:ring-blue-500' to={'/cart'}>
-          Cancel
-          </Link>
-        </div>
-
-        {/* Order Summary */}
-        <div className="mt-8 p-4 shadow-2xl  bg-white shadow-slate-900 rounded-2xl">
-          <h2 className="text-xl font-bold text-but mb-4">Order Summary</h2>
-          <p className="text-black"><strong>Total Cost:</strong> ${totalCost?.toLocaleString()}</p>
-          {installmentPlan && (
-            <>
-              <p className="text-black"><strong>Down Payment:</strong> ${parseFloat(downPayment).toLocaleString()}</p>
-              <p className="text-black"><strong>Installment Plan:</strong> {installmentPlan || 'N/A'} months</p>
-              <p className="text-black"><strong>Monthly Installment:</strong> ${monthlyInstallment?.toLocaleString()}</p>
-            </>
-          )}
-          <p className="text-black"><strong>Delivery Option:</strong> {deliveryOption || 'N/A'}</p>
-          <p className="text-black"><strong>Payment Method:</strong> {paymentMethod || 'N/A'}</p>
-        </div>
       </div>
     </div>
   );
